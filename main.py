8c3f978c37a5c59fdacbc0e36e39be74a2aadb20
Zain Khan <zain.9496@gmail.com>
2024-01-25 18:40:13 +0500

    Added some UI elements to the main.py

--- main.py
+++ main.py
@@ -3,8 +3,38 @@ import streamlit as st
 import time
 import random
 import numpy as np
+from Solver import Solver
+from PIL import Image
+
+thread_art_solver = Solver()
+
+def GenerateClicked():
+	print("Pressed Generate")
+
+
+with st.sidebar:
+	uploaded_img = st.file_uploader("Choose Image", type=['png', 'jpg', 'jpeg', 'PNG', 'JPG'], accept_multiple_files=False, key=None)
+	if uploaded_img is not None:
+		image = Image.open(uploaded_img)
+		img_array = np.array(image)
+
+
+		st.image(
+	        img_array,
+	        caption=f"You amazing image has shape {img_array.shape[0:2]}",
+	        use_column_width=True,
+    	)
+    	
+	st.button("Generate Thread Art", key=None,  on_click=GenerateClicked, use_container_width = True)
+
+
+
+
+
 
 my_placeholder = st.empty()
+
+
 
 while True:
 	img = np.zeros((500, 500, 3)).astype("uint8")
